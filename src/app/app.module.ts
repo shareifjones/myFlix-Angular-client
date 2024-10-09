fde9f9e59141ce6821a2538d7ea90d15286b6ed9
shareifjones <shareifjones@gmail.com>
2024-10-08 22:56:45 -0400

    working on 6.3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatInputModule } from '@angular/material/input';
--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+// You'll use this import to close the dialog on success
+import { MatDialogRef } from '@angular/material/dialog';
+
+// This import brings in the API calls we created in 6.2
+import { UserRegistrationService } from '../fetch-api-data.service';
+
+// This import is used to display notifications back to the user
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-user-registration-form',
@@ -7,9 +16,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserRegistrationFormComponent implements OnInit {
 
-  constructor() { }
+  @Input() userData = { Username: '', Password: '', Email: '', Birthday: '' };
+
+  constructor(
+    public fetchApiData: UserRegistrationService,
+    public dialogRef: MatDialogRef<UserRegistrationFormComponent>,
+    public snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
+  }
+
+  // This is the function responsible for sending the form inputs to the backend
+  registerUser(): void {
+    this.fetchApiData.userRegistration(this.userData).subscribe((response) => {
+      console.log(response)
+      this.dialogRef.close();  // This will close the modal on success!
+      this.snackBar.open('User registration successful', 'OK', {
+        duration: 2000
+      })
+    }, (response) => {
+      this.snackBar.open(response, 'OK', {
+        duration: 2000
+      });
+    });
   }
 
 }
